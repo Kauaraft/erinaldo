e1b2e2dccfe353c75496b07469413fb5c1fd1096
Kauaraft <163850929+Kauaraft@users.noreply.github.com>
2024-06-20 11:55:12 -0300

    Update Cidadesrec.java

--- Cidadesrec.java
+++ Cidadesrec.java
@@ -1,4 +1,22 @@
+import java.util.Scanner;
 
 public class Cidadesrec {
-
+	Scanner sc = new Scanner (System.in);
+	String nome;
+	int identificador, veiculodepasseio, qntddeacidentes;
+	
+	public Cidadesrec (String nome ) {
+		System.out.println("Qual o nome da cidade ?: ");
+		nome = sc.next();
+		
+		System.out.println("Qual o código da cidade ?: ");
+		identificador = sc.nextInt();
+		
+		System.out.println("Quantos veiculos de passeio tem na cidade ?: ");
+		veiculodepasseio = sc.nextInt();
+		
+		System.out.println("Qual a quantidade de acidentes?: ");
+		qntddeacidentes = sc.nextInt();
+	}
 }
+
